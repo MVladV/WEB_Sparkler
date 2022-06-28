e0e9830e1278f7f3be5217cf29aea28e563ba818
Vlad Mitlytskyi <90638567+MVladV@users.noreply.github.com>
2022-06-28 20:33:52 +0300

    Add files via upload

---
+++ Laravel_Project/sparkler/database/migrations/2022_06_27_125205_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->text('surname');
+            $table->text('email');
+            $table->integer('order_num');
+            $table->integer('furniture_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
